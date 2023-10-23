6c61ea9d37e89e8eb6639dd2bab0e881f91dfd72
Valerio Ciccolella <ciccolella1acat@gmail.com>
2023-10-23 11:23:03 +0200

    IMPLEMENTAZIONE utente

--- src/entities/GestioneAuto.java
+++ src/entities/GestioneAuto.java
@@ -1,11 +1,14 @@
 package entities;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class GestioneAuto {
 	
 	private ArrayList<Auto> listaAuto;
 	private String nomeFile;
+	private Utente utente;
+	private Scanner scanner;
 	
 	public GestioneAuto(ArrayList<Auto> listaAuto, String nomeFile) {
 		this.listaAuto = listaAuto;
@@ -27,6 +30,43 @@ public class GestioneAuto {
 	public void setNomeFile(String nomeFile) {
 		this.nomeFile = nomeFile;
 	}
+	
+	//METODO MANAGER GESTIONE UTENTE
+	public void displayOpzioni() {
+		Scanner scanner = new Scanner(System.in);
+		boolean exit = false;
+		
+		while (!exit) {
+			System.out.println("Benvenuto nella gestioneAuto!");
+			System.out.println("1. Accedi");
+			System.out.println("2. Esci");
+			System.out.println("Risposta: ");
+
+			int scelta = scanner.nextInt();
+			scanner.nextLine();
+
+			switch (scelta) {
+			case 1:
+				utente = creaUtente();
+				//IMPLEMENTARE UN METODO CHE RICHIAMI TUTTI I METODI CHE PUò USARE L'UTENTE
+				break;
+			case 2:
+				exit = true;
+				break;
+			default:
+				System.out.println("Scelta non valida. Riprova.");
+			}
+		}
+	}
+	
+	//CREAZIONE UTENTE
+		private Utente creaUtente() {
+			System.out.println("Inserisci il nome dell'utente:");
+	        String nomeAdmin = scanner.nextLine();
+	        System.out.println("Inserisci password dell'utente:");
+	        String password = scanner.nextLine();
+	        return new Utente(nomeAdmin, password);
+		}
 	
 	//METODI GESTIONE AUTO
 	public void aggiungiAuto(Auto auto) {
