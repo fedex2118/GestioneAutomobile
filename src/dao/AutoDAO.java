a9f91fda73b909d0892687ee4fb9c2c5ddeb2ba6
Valerio Ciccolella <ciccolella1acat@gmail.com>
2023-10-23 12:07:28 +0200

    create Dao and methods

---
+++ src/dao/AutoDAO.java
@@ -0,0 +1,34 @@
+package dao;
+
+import java.util.ArrayList;
+
+import entities.Auto;
+import entities.GestioneAuto;
+import gestoreConnessioni.GestoreConnessioni;
+
+public class AutoDAO {
+	public static ArrayList<Auto> getAllAuto(GestoreConnessioni gc, GestioneAuto ga) {
+		return null;
+
+	}
+
+	public static ArrayList<Auto> insertAuto(GestoreConnessioni gc, GestioneAuto ga) {
+		return null;
+
+	}
+
+	public static ArrayList<Auto> modificaAuto(GestoreConnessioni gc, GestioneAuto ga) {
+		return null;
+
+	}
+
+	public static ArrayList<Auto> deleteAuto(GestoreConnessioni gc, GestioneAuto ga) {
+		return null;
+
+	}
+
+	public static ArrayList<Auto> cercaAutoPerTarga(GestoreConnessioni gc, GestioneAuto ga) {
+		return null;
+
+	}
+}
