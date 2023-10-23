ac34b4fb9783641ad5e3a4a90086e68548b67405
Valerio Ciccolella <ciccolella1acat@gmail.com>
2023-10-23 12:34:22 +0200

    metodo visualizzaAuto

--- src/dao/AutoDAO.java
+++ src/dao/AutoDAO.java
@@ -1,5 +1,7 @@
 package dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import entities.Auto;
@@ -8,7 +10,30 @@ import gestoreConnessioni.GestoreConnessioni;
 
 public class AutoDAO {
 	public static ArrayList<Auto> getAllAuto(GestoreConnessioni gc, GestioneAuto ga) {
-		return null;
+		String query = "SELECT * FROM auto";
+
+		ResultSet rs = gc.eseguiStatementQuery(query);
+
+		ArrayList<Auto> automobili = ga.getListaAuto();
+
+		try {
+			while (rs.next()) {
+				int id = Integer.parseInt(rs.getString(1));
+				String marca = rs.getString(2);
+				String modello = rs.getString(3);
+				int anno = Integer.parseInt(rs.getString(4));
+				String targa = rs.getString(5);
+				float prezzo = Float.parseFloat(rs.getString(6));
+				String tipoCarburante = rs.getString(7);
+				Auto autombile = new Auto(id, marca, modello, anno, targa, prezzo, tipoCarburante);
+				automobili.add(autombile);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return automobili;
 
 	}
 
