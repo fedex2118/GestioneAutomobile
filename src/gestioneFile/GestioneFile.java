e5b333bc42ac31c8a4557adff0b661bdac1efb6e
Federico Ziegler <105019871+fedex2118@users.noreply.github.com>
2023-10-23 22:06:04 +0200

    gestione file with InputStream

--- src/gestioneFile/GestioneFile.java
+++ src/gestioneFile/GestioneFile.java
@@ -2,28 +2,35 @@ package gestioneFile;
 
 import java.nio.file.*;
 import java.io.*;
+import java.net.URL;
 
 
 public class GestioneFile {
 
 
-    private static final String RESOURCES_FOLDER = "resources/";
+//    private static final String RESOURCES_FOLDER = "/resources/";
+    
+    public static final String PATH = "/resources/DBAutomobili.txt";
 
 
-    public static void leggiFile(Path path) throws IOException {
-        path = Paths.get(RESOURCES_FOLDER + path.toString());
-        try (BufferedReader reader = Files.newBufferedReader(path)) {
-            String currentLine = reader.readLine();
-            System.out.println(currentLine);
-        }
-    }
+//    public static void leggiFile(Path path) throws IOException {
+//        path = Paths.get(RESOURCES_FOLDER + path.toString());
+//        try (BufferedReader reader = Files.newBufferedReader(path)) {
+//            String currentLine = reader.readLine();
+//            System.out.println(currentLine);
+//        }
+//    }
 
 
     public static void leggiFile(String path) throws IOException {
-        path = RESOURCES_FOLDER + path;
-        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
-            String currentLine = reader.readLine();
-            System.out.println(currentLine);
+        try (InputStream resource = GestioneFile.class.getResourceAsStream(path)) {
+        	BufferedReader reader = new BufferedReader(new InputStreamReader(resource));
+        	String currentLine = reader.readLine();
+        	System.out.println(currentLine);
+        	
+        	reader.close();
+        } catch(Exception e) {
+        	e.printStackTrace();
         }
     }
 
@@ -31,8 +38,8 @@ public class GestioneFile {
     // Metodo principale per testare
     public static void main(String[] args) {
         try {
-            leggiFile(Paths.get("DBAutomobili.txt"));
-            leggiFile("DBAutomobili.txt");
+            leggiFile(PATH);
+//            leggiFile("DBAutomobili.txt");
         } catch (IOException e) {
             e.printStackTrace();
         }
